1b87f2392962251c3e1359113a407b84102ad6b5
hakantan65-droid <hakantan65@gmail.com>
2026-03-22 07:16:43 +0000

    kayit_sistemi.py

---
+++ antrenman.py
@@ -0,0 +1,9 @@
+# Basit bir antrenman kayıt sistemi denemesi
+antrenman_adi = input("Bugünkü antrenmanın ana konusu nedir?: ")
+oyuncu_sayisi = input("Kaç oyuncu katıldı?: ")
+notlar = input("Antrenör notunuz: ")
+
+print("\n--- GÜNLÜK RAPOR OLUŞTURULDU ---")
+print(f"Konu: {antrenman_adi}")
+print(f"Katılım: {oyuncu_sayisi} Oyuncu")
+print(f"Not: {notlar}")
