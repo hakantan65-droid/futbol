4f07599c17e37ca77ed66f74c2421e027f0a5afb
hakantan65-droid <hakantan65@gmail.com>
2026-04-07 11:20:17 +0300

    Android WebView projesi ile APK altyapısı ekle

---
+++ android-app/app/src/main/java/com/futbolokulum/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.futbolokulum
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val webView = WebView(this)
+        setContentView(webView)
+
+        webView.webChromeClient = WebChromeClient()
+        webView.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            databaseEnabled = true
+            cacheMode = WebSettings.LOAD_DEFAULT
+            allowFileAccess = true
+            allowContentAccess = true
+        }
+
+        webView.loadUrl("file:///android_asset/index.html")
+    }
+}
---
+++ android-app/settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "FutbolOkulum"
+include(":app")
